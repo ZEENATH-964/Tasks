604a53ae3e8fbf6e1406505f682245d7eec6c830
ZEENATH-964 <munavarbr@gmail.com>
2024-09-27 16:20:32 +0530

    first commit

---
+++ task1.dart
@@ -0,0 +1,12 @@
+void main(){
+    int x=3;
+    int y=4;
+    int z=5;
+    
+   print("before swapping:x=$x,y=$y,z=$z");
+   int temp=x;
+   x=y;
+   y=z;
+   z=temp;
+   print("after swapping:x=$x,y=$y,z=$z");
+}

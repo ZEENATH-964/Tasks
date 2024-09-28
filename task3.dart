ff4424f242d28d79d8ada7feca41703b138a460a
ZEENATH-964 <munavarbr@gmail.com>
2024-09-28 21:34:58 +0530

    f commit

--- task3.dart
+++ task3.dart
@@ -1,9 +1,1 @@
-  void main() {
- for(int i=1;i<=5;i++){
-   String a="";
-   for(int  j=1;j<=i;j++){
-     a+="$j";
-   }
-   print(a);
- }
-}
+  
---
+++ task4.dart
@@ -0,0 +1,9 @@
+void main() {
+ for(int i=1;i<=5;i++){
+   String a="";
+   for(int  j=1;j<=i;j++){
+     a+="$j";
+   }
+   print(a);
+ }
+}

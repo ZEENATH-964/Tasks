0a70697ab978d03499651544054d80fbc785e2b9
ZEENATH-964 <munavarbr@gmail.com>
2024-09-30 14:12:23 +0530

    f commit

--- task5.dart
+++ task5.dart
@@ -1,11 +1,27 @@
 void main() {
   for(int i=0;i<=5;i++){
-   int alpha=65;                             
+     int alpha=65;                             
     String a="";
-    for(int j=0;j<=i;j++){
-      a+="${String.fromCharCode(alpha+j)}";
+    for(int j=5;j>i;j--){
+      a+=" ";
+    }
+     for(int k=0;k<=i;k++){
+      a+="${String.fromCharCode(alpha+k)} ";
     }
 print(a);
   }
     
-}
+ for(int i=0;i<=5;i++){
+     int alpha=65;                             
+    String a="";
+    for(int j=0;j<=i;j++){
+      a+=" ";
+    }
+     for(int k=4-i;k>=0;k--){
+      a+="${String.fromCharCode(alpha++)} ";
+    }
+print(a);
+ }
+    
+  } 
+  
